81d962edee8f38dce42fb5584344bcc7dc9d11da
Tarek-Mzoughi <tarekest2307@gmail.com>
2025-10-07 01:03:56 +0100

    kkk

--- src/main/java/com/iset/dao/OffreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.iset.dao;
-
-import com.iset.entities.Offre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OffreRepository extends JpaRepository<Offre, Long> {
-
-}
